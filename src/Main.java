b4bd95197042dd19b2650f47629ce72afbe52e06
HUANGmin1211 <1002928984@qq.com>
2019-06-05 09:41:37 +0800

    利用&&短路做递归求和

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,14 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
+/**
+ * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ */
 public class Main {
-    public int TreeDepth(TreeNode root) {
+    public static void main(String[] args) {
 
-
-        return press(root,0);
-    }
-
-    private int press(TreeNode root, int deepth) {
-        if (root == null)
-            return deepth;
-
-        return Math.max(press(root.left, deepth+1),press(root.right, deepth+1));
-    }
-
-    @Test
-    public void test(){
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
     }
 }
---
+++ src/nowcoder/Sum.java
@@ -0,0 +1,16 @@
+package nowcoder;
+
+/**
+ * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ */
+public class Sum {
+    public int Sum_Solution(int n) {
+        return press(n);
+    }
+
+    private int press(int n) {
+        int sum = n;
+        boolean b = (n != 0) && (sum += press(n - 1)) != 0;
+        return sum;
+    }
+}
