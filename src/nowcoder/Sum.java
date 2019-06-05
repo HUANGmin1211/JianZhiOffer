f9f2b9b989a1d40afa146befee5a9278e16b86a4
HUANGmin1211 <1002928984@qq.com>
2019-06-05 09:48:43 +0800

    利用 与 和 亦或 求和

--- src/nowcoder/Sum.java
+++ src/nowcoder/Sum.java
@@ -1,9 +1,8 @@
 package nowcoder;
 
-/**
- * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
- */
+
 public class Sum {
+    // 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
     public int Sum_Solution(int n) {
         return press(n);
     }
@@ -13,4 +12,22 @@ public class Sum {
         boolean b = (n != 0) && (sum += press(n - 1)) != 0;
         return sum;
     }
+
+    // 不用加减乘除求和
+    public int Add(int num1, int num2) {
+        int yiHuo = num1 ^ num2;
+        int yu = num1 & num2;
+
+        if (yu == 0) {
+            return yiHuo;
+        } else {
+            yu <<= 1;
+            return Add(yiHuo, yu);
+        }
+
+    }
+
+
+
+
 }
