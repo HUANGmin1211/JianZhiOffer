6bbf7a06612001c676ccd1598e21b9cced86f580
HUANGmin1211 <1002928984@qq.com>
2019-06-09 22:02:01 +0800

    🐸跳台阶的非递归版本

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,69 @@ import java.util.HashSet;
 import java.util.LinkedList;
 
 /**
- * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ * 地上有一个m行和n列的方格。
+ * 一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格，
+ * 但是不能进入行坐标和列坐标的数位之和大于k的格子。
+ * 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。
+ * 但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
  */
 public class Main {
 
+    public int movingCount(int threshold, int rows, int cols) {
+        if (threshold < 0)
+            return 0;
 
+        int[][] path = new int[rows][cols];
+        press(0, 0, rows, cols, threshold, path);
 
+        int count = 0;
+        for (int i = 0; i < path.length; i++) {
+            for (int j = 0; j < path[0].length; j++) {
+                if (path[i][j] == 1)
+                    count++;
+            }
+        }
+        return count;
+    }
 
-}
-//class TreeLinkNode {
-//    int val;
-//    TreeLinkNode left = null;
-//    TreeLinkNode right = null;
-//    TreeLinkNode next = null;
-//
-//    TreeLinkNode(int val) {
-//        this.val = val;
-//    }
-//}
+    private void press(int i, int j, int rows, int cols, int threshold, int[][] path) {
+        path[i][j] = 1;
+        if (i > 0 && canEnter(i - 1, j, threshold) && path[i - 1][j] == 0) {
+            path[i - 1][j] = 1;
+            press(i - 1, j, rows, cols, threshold, path);
+        }
+        if (i < rows - 1 && canEnter(i + 1, j, threshold) && path[i + 1][j] == 0) {
+            path[i + 1][j] = 1;
+            press(i + 1, j, rows, cols, threshold, path);
+        }
+        if (j > 0 && canEnter(i, j - 1, threshold) && path[i][j - 1] == 0) {
+            path[i][j - 1] = 1;
+            press(i, j - 1, rows, cols, threshold, path);
+        }
+        if (j < cols - 1 && canEnter(i, j + 1, threshold) && path[i][j + 1] == 0) {
+            path[i][j + 1] = 1;
+            press(i, j + 1, rows, cols, threshold, path);
+        }
+    }
 
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
+    private boolean canEnter(int i, int j, int threshold) {
+        String si = String.valueOf(i);
+        String sj = String.valueOf(j);
 
-    public TreeNode(int val) {
-        this.val = val;
+        int sum = 0;
+        for (int m = 0; m < si.length(); m++) {
+            sum += Integer.valueOf(si.charAt(m) + "");
+        }
+        for (int m = 0; m < sj.length(); m++) {
+            sum += Integer.valueOf(sj.charAt(m) + "");
+        }
 
+        return sum <= threshold;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(movingCount(15, 20, 20));
     }
 
 }
--- src/nowcoder/StupidFrog.java
+++ src/nowcoder/StupidFrog.java
@@ -17,6 +17,34 @@ public class StupidFrog {
     }
 
     /**
+     * ★★★题目一的非递归版本★★★
+     */
+    public int climbStairs(int n) {
+        if (n < 3)
+            return n;
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+
+    /**
+     * 题目一
+     */
+    private int press1(int target) {
+        if (target == 1)
+            return 1;
+        if (target == 2)
+            return 2;
+        else
+            return press1(target - 1) + press1(target - 2);
+    }
+
+    /**
      * 题目二
      * */
     private int press2(int target) {
@@ -24,18 +52,6 @@ public class StupidFrog {
             return 1;
         else
             return 2 * press2(target-1);
-    }
-
-    /**
-     * 题目一
-     * */
-    private int press1(int target) {
-        if (target == 1)
-            return 1;
-        if (target == 2)
-            return 2;
-        else
-            return press1(target-1) + press1(target-2);
     }
 
 }
